e67070c6a99392c0067023e9fdf6daabd908f32b
Isaiah B <thechatlolz@gmail.com>
2023-12-26 12:50:03 -0500

    Demo Angular with Colors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
 
+interface Color {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  selectedColor: string
   title = 'DismasDemo';
+  colors: Color[] = [
+    {value: 'red', viewValue: 'Red'},
+    {value: 'blue', viewValue: 'Blue'},
+    {value: 'green', viewValue: 'Green'},
+    {value: 'aqua', viewValue: 'Aqua'},
+
+  ];
+
+
+  divStyles: any = {
+    'position': 'fixed',
+    'top': '0',
+    'left': '0',
+    'width': '100%',
+    'height': '100%',
+    'backgroundColor': 'white'
+  };
+
+  changeColor() {
+    this.divStyles.backgroundColor = this.selectedColor;
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatSelectModule} from '@angular/material/select';
+
+
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatSelectModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
